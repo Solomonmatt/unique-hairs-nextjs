0089a5e5ec6ae2833879d1726ea7ff599e5879b6
Mathew Oreoluwa Solomon <100151674+Solomonmatt@users.noreply.github.com>
2025-01-13 10:38:11 +0100

    Update loading.jsx

--- app/loading.jsx
+++ app/loading.jsx
@@ -2,11 +2,17 @@
 
 import { FallingLines } from "react-loader-spinner";
 
-export default function Loader() {
+interface LoaderProps {
+  color?: string;
+  width?: string;
+  visible?: boolean;
+}
+
+export default function Loader({ color = "#222", width = "80", visible = true }: LoaderProps) {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center gap-3">
       <div className="flex flex-row items-baseline">
-        <FallingLines color="#222" width="80" visible={true} />
+        <FallingLines color={color} width={width} visible={visible} />
       </div>
     </div>
   );
